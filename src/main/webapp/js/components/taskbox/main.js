856157e33020112d672d8592dc89ff5b7aca1790
Erlend Didrik Aas <erlendaas@158-37-193-114.hvl.no>
2021-09-14 10:25:36 +0200

    hallveis i taskbox

--- src/main/webapp/js/components/taskbox/main.js
+++ src/main/webapp/js/components/taskbox/main.js
@@ -1,3 +1,99 @@
 export default class extends HTMLElement {
+
+	shadow;
+	css = "js/components/taskbox/taskbox.css";
+	allstatuses;
+	taskbox;
+	newtaskCallbackList = new Map();
+	newTaskCallbackId = 0;
 	
+
+
+
+	constructor() {
+		super();
+		this.shadow = this.attachShadow({ mode: 'closed' });
+		this.createLink();
+		this.taskbox = this.createModal();
+		this.taskbox.querySelector("span").addEventListener("click", this.close.bind(this));
+		//this.taskbox.querySelector("button").addEventListener("click", this..bind(this));
+
+	}
+
+
+	createModal() {
+
+		const modal = document.createElement('div');
+		modal.classList.add("hidden");
+		//modal.classList.add("modal")
+		//modal.className += "modal";
+
+		const content = `
+		
+  			<div class="modal-content">
+   				<span class="close">&times;</span>
+				<label for="taskTitle">Title: </label>
+   				<input id="taskTitle" type="text" placeholder="Title"></input><br><br>
+				<label for="status">Status: </label>
+				<select id="status" >
+					<option value="waiting">WAITING</option>
+					<option value="active">ACTIVE</option>
+					<option value="done">DONE</option>
+				</select><br><br>
+				<button type="button">Add task</button>
+			</div>
+		`;
+
+		modal.insertAdjacentHTML('beforeend', content);
+		this.shadow.appendChild(modal);
+
+		return modal;
+
+
+	}
+
+	show() {
+
+		this.taskbox.classList.remove("hidden");		
+
+	}
+
+
+	newtaskCallback(callback) { 
+		
+		this.newtaskCallbackList.set(this.newTaskCallbackId, callback);
+		this.newTaskCallbackId++;
+	
+	}
+
+
+	close() { 
+		
+		this.taskbox.classList.add("hidden");		
+		
+	}
+
+
+
+
+
+	createLink() {
+
+		const link = document.createElement('link');
+
+		link.rel = 'stylesheet';
+
+		link.type = 'text/css';
+
+
+
+		link.href = this.css;
+
+		// Append link element to HTML head
+		this.shadow.appendChild(link);
+	}
+
+
+
+
 }
--- src/main/webapp/js/controller.js
+++ src/main/webapp/js/controller.js
@@ -1,9 +1,13 @@
 import TaskList from './components/tasklist/main.js';
+import TaskBox from './components/taskbox/main.js';
+
 
 
 const controller = {
 	run() {
 		customElements.define('task-list', TaskList);
+		customElements.define('task-box', TaskBox);
+
 
 		const tasklist = document.querySelector("task-list");
 		tasklist.enableaddtask();
@@ -11,13 +15,20 @@ const controller = {
 			() => { console.log("Click event on 'New task button'") }
 		);
 
+		const taskbox = document.querySelector("task-box");
+		
+		taskbox.show();
+		
+		//taskbox.close()
+
+
 
 		const task = {
 			"id": 5,
 			"title": "Do DAT152 home work",
 			"status": "ACTIVE"
 		};
-		
+
 		tasklist.showTask(task);
 	},
 
